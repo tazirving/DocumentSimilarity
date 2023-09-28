e42cba337656e701ca88809ff5191e4b9db07fca
Angelica Valdovinos <angelica.gonzalez.valdovinos2@gmail.com>
2023-09-27 22:41:28 -0600

    "librerias"

--- prac3.py
+++ prac3.py
@@ -1,8 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
-import pandas as pd
-import re
+
 
 #Cargando el corpus
 with open("noticias_nuevo_corpus_2.txt", "r") as txt_file:
